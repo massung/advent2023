9725c8ec859d1581c8b376f1642d9d4d35c357ea
Jeffrey Massung <massung@gmail.com>
2023-12-06 22:40:49 -0600

    Day 6 -- fast.

--- day6/day6.rb
+++ day6/day6.rb
@@ -17,16 +17,32 @@ class Race
 	
 	# slow solution -- just try them all
 	def distances
-		(1...@time).filter {|t| distance(t) if distance(t) > @distance}
+		(1...@time).count {|t| distance(t) if distance(t) > @distance}
+	end
+	
+	# fast solution: -t^2 + T*t - D = 0
+	def distances_fast
+		a = -1
+		b = @time
+		c = -@distance
+		
+		# radical
+		r = b*b - 4*a*c
+		
+		# always 2 solutions due to the nature of the problem
+		x1 = ((-b + Math.sqrt(r)) / 2*a).to_i + 1
+		x2 = ((-b - Math.sqrt(r)) / 2*a).to_i
+		
+		x2 - x1 + 1
 	end
 end
 
 part1 = times.zip(distances).map do |t, d|
-	Race.new(t, d).distances.length
+	Race.new(t, d).distances_fast
 end
 
 # part 1
 p part1.reduce :*
 
 # part 2
-p Race.new(times.join.to_i, distances.join.to_i).distances.length
+p Race.new(times.join.to_i, distances.join.to_i).distances_fast
